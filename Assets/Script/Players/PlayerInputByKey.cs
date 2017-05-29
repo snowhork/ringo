e7fef45137bffef69738da0456db97247cb1e6e9
kyoh <stayheresilence@gmail.com>
2017-05-29 17:03:33 +0900

    fix special item

--- Assets/Script/Players/PlayerInputByKey.cs
+++ Assets/Script/Players/PlayerInputByKey.cs
@@ -16,16 +16,19 @@ namespace Script.Players
 
         public Point AttackInput()
         {
-            if (Input.GetKey(KeyCode.D)) return new Point(1,  0);
-            if (Input.GetKey(KeyCode.A))  return new Point(-1, 0);
-            if (Input.GetKey(KeyCode.W))   return new Point(0, 1);
-            if (Input.GetKey(KeyCode.S))   return new Point(0, -1);
+            if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.LeftShift)) return new Point(1,  0);
+            if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.LeftShift)) return new Point(-1, 0);
+            if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.LeftShift)) return new Point(0, 1);
+            if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.LeftShift)) return new Point(0, -1);
             return new Point(0, 0);
         }
 
         public Point SpecialAttackInput()
         {
-            if (Input.GetKey(KeyCode.P))  return new Point(-1, 0);
+            if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.LeftShift)) return new Point(1,  0);
+            if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.LeftShift)) return new Point(-1, 0);
+            if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift)) return new Point(0, 1);
+            if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.LeftShift)) return new Point(0, -1);
             return new Point(0, 0);
         }
     }
